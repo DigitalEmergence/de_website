ab3386eb51f3e8eb9c1b9d8b62c40617555ef7e0
yasemo <yaseenmoore@gmail.com>
2025-04-05 00:49:45 -0400

    new edits

--- shared/dependencies.js
+++ shared/dependencies.js
@@ -4,6 +4,7 @@ registerDependencies$({
     'homepage': '/de_website/client/components/pages/home/home.js',
     'jspherepage': '/de_website/client/components/pages/jsphere/jsphere.js',
     'elementjspage': '/de_website/client/components/pages/elementjs/elementjs.js',
+    'aboutpage': '/de_website/client/components/pages/about/about.js',
     'carousel': '/de_website/client/components/widgets/carousel/carousel.js',
     'accordion': '/de_website/client/components/widgets/accordion/accordion.js',
     'navbar': '/de_website/client/components/widgets/navbar/navbar.js',
